b93ce572e30f25eaeefa2329acbed6cde6568b1b
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-30 23:03:53 +0100

    add_hover

--- aldeiapintada-app/components/Footer.tsx
+++ aldeiapintada-app/components/Footer.tsx
@@ -12,18 +12,20 @@ export default function Footer() {
       </div>
      <div className="flex mr-6 justify-center items-center absolute  right-0">
         <div className="flex flex-col justify-center items-end text-sm ">
-          <p>SEGUE-NOS  </p>
-          <Link href="https://www.facebook.com/aldeiapintada/" target="_blank">
+          <p>SEGUE-NOS </p>
+          
+          <Link className="hover:text-[#00b499]" href="https://www.facebook.com/aldeiapintada/" target="_blank">
             FACEBOOK
           </Link>
-          <Link href="https://www.instagram.com/aldeiapintada/" target="_blank">
+          <Link className="hover:text-[#00b499]" href="https://www.instagram.com/aldeiapintada/" target="_blank">
           INSTAGRAM
           </Link>
+          
         </div>
         <Image
           src="/Logo_pretoBranco.jpeg"
           alt="Logo_preto_e_branco"
-          className="w-[8rem] h-[8rem] object-cover"
+          className="w-[6rem] h-[6rem] object-cover"
           width={2000}
           height={2000}
         />
