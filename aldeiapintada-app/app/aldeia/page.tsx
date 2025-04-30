3b512f853f62f73626e5ac8156a18a149e4f307d
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-30 23:34:29 +0100

    add_photo_to_aldeia

--- aldeiapintada-app/app/aldeia/page.tsx
+++ aldeiapintada-app/app/aldeia/page.tsx
@@ -1,9 +1,20 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Aldeia() {
-    return (
-        <main>
-            <Header />
-        </main>
-    );
+  return (
+    <main className="flex flex-col gap-12 pb-8 relative">
+      <Header />
+
+      <Image
+        src="/Header_Aldeia.JPG"
+        alt="Imagem de fundo da página principal"
+        className="w-full h-[40rem] object-cover absolute"
+        width={3846}
+        height={2555}
+      />
+       <div className="mt-[40rem]"></div>
+    </main>
+   
+  );
 }
