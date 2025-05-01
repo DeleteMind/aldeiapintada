b82526f5ac34bb74b771194fe0ad4f6ad47f8d0d
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-05-01 22:47:16 +0100

    add_photo_at_arquivo

--- aldeiapintada-app/app/arquivo/page.tsx
+++ aldeiapintada-app/app/arquivo/page.tsx
@@ -1,9 +1,20 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Arquivo() {
     return (
-        <main>
+        <main className="flex flex-col relative">
             <Header />
+
+             <Image
+                      src="/Header_arquivo.JPG"
+                      alt=""
+                      className="w-full h-[40rem] object-cover absolute object-[25%_0%]"
+                      width={1600}
+                      height={666}
+                    />
+            
+                    <div className="mt-[35rem]"></div>
         </main>
     );
 }
