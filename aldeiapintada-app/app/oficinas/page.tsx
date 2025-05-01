98c27186658640b3b99a45167b089d47ba7bea8b
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-05-01 22:37:46 +0100

    add_photo_at_oficinas

--- aldeiapintada-app/app/oficinas/page.tsx
+++ aldeiapintada-app/app/oficinas/page.tsx
@@ -1,9 +1,20 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Oficinas() {
     return (
-        <main>
+        <main className="flex flex-col relative">
             <Header />
+  
+        <Image
+          src="/Header_oficinas.jpg"
+          alt=""
+          className="w-full h-[40rem] object-cover absolute object-[25%_15%]"
+          width={1600}
+          height={666}
+        />
+
+        <div className="mt-[35rem]"></div>
         </main>
     );
 }
