e0d1d9fac89d516eba5c5b12d250b1a299448d23
tinomarques <tinoamarques@gmail.com>
2025-10-16 20:02:57 +0100

    Init + upgrade

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="mt-auto  h-full flex justify-center items-center relative">
       
       <div className="">
       <p className="text-[0.70rem] mx-auto text-center pt-4">
-        © 2025 Aldeia Pintada  <br />Todos os direitos reservados
+        © {currentYear} Aldeia Pintada  <br />Todos os direitos reservados
       </p>
       </div>
      <div className="flex mr-6 justify-center items-center absolute  right-0">
