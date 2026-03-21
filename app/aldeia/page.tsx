c0a3c90f2b444c25e5cb80fd0cba4bffbdc7a166
tinomarques <tinoamarques@gmail.com>
2026-03-21 22:59:13 +0000

    Misc: Deploy

--- app/aldeia/page.tsx
+++ app/aldeia/page.tsx
@@ -1,118 +1,117 @@
-import { Button } from '@/components/Button';
-import Header from '@/components/Header';
-import ImageCarousel from '@/components/ImageCarousel';
-import { Text } from '@/components/Text';
-import galeria1 from '@/public/assets/galeria/1.jpg';
-import galeria2 from '@/public/assets/galeria/2.jpg';
-import galeria3 from '@/public/assets/galeria/3.jpg';
-import galeria4 from '@/public/assets/galeria/4.jpg';
-import galeria5 from '@/public/assets/galeria/5.jpg';
-import galeria6 from '@/public/assets/galeria/6.jpg';
-import galeria7 from '@/public/assets/galeria/7.jpg';
-import galeria8 from '@/public/assets/galeria/8.jpg';
-import aldeiaHeader from '@/public/assets/header/aldeia.jpg';
+import { Button } from "@/components/Button";
+import Header from "@/components/Header";
+import ImageCarousel from "@/components/ImageCarousel";
+import { Text } from "@/components/Text";
+import galeria1 from "@/public/assets/galeria/1.jpg";
+import galeria2 from "@/public/assets/galeria/2.jpg";
+import galeria3 from "@/public/assets/galeria/3.jpg";
+import galeria4 from "@/public/assets/galeria/4.jpg";
+import galeria5 from "@/public/assets/galeria/5.jpg";
+import galeria6 from "@/public/assets/galeria/6.jpg";
+import galeria7 from "@/public/assets/galeria/7.jpg";
+import galeria8 from "@/public/assets/galeria/8.jpg";
+import aldeiaHeader from "@/public/assets/header/aldeia.jpg";
 
 export default function Aldeia() {
-	return (
-		<>
-			<Header src={aldeiaHeader} objectPosition={60} invertTextColor={true} />
+  return (
+    <>
+      <Header src={aldeiaHeader} objectPosition={60} invertTextColor={true} />
 
-			<main className='flex flex-col px-6 gap-20 py-20 items-center'>
-				<section className='max-w-5xl'>
-					<Text size='2xl'>
-						<Text size='2xl' weight='medium'>
-							A Aldeia Pintada
-						</Text>{' '}
-						é uma{' '}
-						<Text size='2xl' weight='medium'>
-							Associação Cultural
-						</Text>{' '}
-						que surge na{' '}
-						<Text size='2xl' weight='medium'>
-							Torre{' '}
-						</Text>{' '}
-						da Magueixa, uma aldeia do concelho da Batalha, hoje conhecida apenas por
-						Torre,{' '}
-						<Text size='2xl' weight='medium'>
-							um lugar singular e com muito para contar.
-						</Text>{' '}
-						A ideia nasce da vontade de documentar o seu{' '}
-						<Text size='2xl' weight='medium'>
-							património
-						</Text>{' '}
-						e usá-lo como{' '}
-						<Text size='2xl' weight='medium'>
-							estímulo para a criação artística e programação cultural.
-						</Text>{' '}
-						<br />
-						<br />
-						As{' '}
-						<Text size='2xl' weight='medium'>
-							histórias, as lendas, os cantares ou as vivências,
-						</Text>{' '}
-						recolhidas em{' '}
-						<Text size='2xl' weight='medium'>
-							diálogo com os habitantes,
-						</Text>{' '}
-						são a base para intervenções como a{' '}
-						<Text size='2xl' weight='medium'>
-							pintura de murais,
-						</Text>{' '}
-						o{' '}
-						<Text size='2xl' weight='medium'>
-							vídeo, a música ou a instalação.
-						</Text>{' '}
-						Para além de{' '}
-						<Text size='2xl' weight='medium'>
-							presentear as pessoas da terra,
-						</Text>{' '}
-						este projeto pretende também{' '}
-						<Text size='2xl' weight='medium'>
-							dar a conhecer melhor o lugar ao forasteiro
-						</Text>{' '}
-						que por ali passa,{' '}
-						<Text size='2xl' weight='medium'>
-							valorizando a memória e identidade local
-						</Text>{' '}
-						ou simplesmente{' '}
-						<Text size='2xl' weight='medium'>
-							trazer cor à aldeia.
-						</Text>
-					</Text>
-				</section>
+      <main className="flex flex-col px-6 gap-20 py-20 items-center">
+        <section className="max-w-5xl">
+          <Text size="2xl">
+            <Text size="2xl" weight="medium">
+              A Aldeia Pintada
+            </Text>{" "}
+            é uma{" "}
+            <Text size="2xl" weight="medium">
+              Associação Cultural
+            </Text>{" "}
+            que surge na{" "}
+            <Text size="2xl" weight="medium">
+              Torre{" "}
+            </Text>{" "}
+            da Magueixa, uma aldeia do concelho da Batalha, hoje conhecida apenas por Torre,{" "}
+            <Text size="2xl" weight="medium">
+              um lugar singular e com muito para contar.
+            </Text>{" "}
+            A ideia nasce da vontade de documentar o seu{" "}
+            <Text size="2xl" weight="medium">
+              património
+            </Text>{" "}
+            e usá-lo como{" "}
+            <Text size="2xl" weight="medium">
+              estímulo para a criação artística e programação cultural.
+            </Text>{" "}
+            <br />
+            <br />
+            As{" "}
+            <Text size="2xl" weight="medium">
+              histórias, as lendas, os cantares ou as vivências,
+            </Text>{" "}
+            recolhidas em{" "}
+            <Text size="2xl" weight="medium">
+              diálogo com os habitantes,
+            </Text>{" "}
+            são a base para intervenções como a{" "}
+            <Text size="2xl" weight="medium">
+              pintura de murais,
+            </Text>{" "}
+            o{" "}
+            <Text size="2xl" weight="medium">
+              vídeo, a música ou a instalação.
+            </Text>{" "}
+            Para além de{" "}
+            <Text size="2xl" weight="medium">
+              presentear as pessoas da terra,
+            </Text>{" "}
+            este projeto pretende também{" "}
+            <Text size="2xl" weight="medium">
+              dar a conhecer melhor o lugar ao forasteiro
+            </Text>{" "}
+            que por ali passa,{" "}
+            <Text size="2xl" weight="medium">
+              valorizando a memória e identidade local
+            </Text>{" "}
+            ou simplesmente{" "}
+            <Text size="2xl" weight="medium">
+              trazer cor à aldeia.
+            </Text>
+          </Text>
+        </section>
 
-				<ImageCarousel
-					imageHeight={400}
-					images={[
-						{ src: galeria1, alt: 'Galeria imagem 1' },
-						{ src: galeria2, alt: 'Galeria imagem 2' },
-						{ src: galeria3, alt: 'Galeria imagem 3' },
-						{ src: galeria4, alt: 'Galeria imagem 4' },
-						{ src: galeria5, alt: 'Galeria imagem 5' },
-						{ src: galeria6, alt: 'Galeria imagem 6' },
-						{ src: galeria7, alt: 'Galeria imagem 7' },
-						{ src: galeria8, alt: 'Galeria imagem 8' },
-					]}
-				/>
+        <ImageCarousel
+          imageHeight={400}
+          images={[
+            { src: galeria1, alt: "Galeria imagem 1" },
+            { src: galeria2, alt: "Galeria imagem 2" },
+            { src: galeria3, alt: "Galeria imagem 3" },
+            { src: galeria4, alt: "Galeria imagem 4" },
+            { src: galeria5, alt: "Galeria imagem 5" },
+            { src: galeria6, alt: "Galeria imagem 6" },
+            { src: galeria7, alt: "Galeria imagem 7" },
+            { src: galeria8, alt: "Galeria imagem 8" },
+          ]}
+        />
 
-				<section className='flex flex-col items-center gap-20 max-w-5xl'>
-					<Text size='5xl' weight='bold'>
-						MAPA DA ALDEIA
-					</Text>
-					<Text size='xl'>
-						Permite a descoberta de todas as pinturas murais realizadas até ao
-						momento, bem como de informações sobre as mesmas. Leva-nos também a lugares
-						que destacamos na nossa aldeia como “pontos de interesse”, como eiras para
-						apreciar a natureza ou uma tasca para um cafezinho.
-					</Text>
-					<Button
-						href='https://www.google.com/maps/d/u/0/viewer?mid=12uQ-54JxIuYFFuS8r99oAsB_rAsRxPbt&ll=39.65727979629874%2C-8.761598399999992&z=15&fbclid=PAAaaVJbs99rNer1I8PKnqJ8QnBj9PV6_zutPiZ79WWmN-KPMBzQCqVdYrDDg'
-						newTab={true}
-					>
-						Ver no Google Maps
-					</Button>
-				</section>
-			</main>
-		</>
-	);
+        <section className="flex flex-col items-center gap-20 max-w-5xl">
+          <Text size="5xl" weight="bold">
+            MAPA DA ALDEIA
+          </Text>
+          <Text size="xl">
+            Permite a descoberta de todas as pinturas murais realizadas até ao momento, bem como de
+            informações sobre as mesmas. Leva-nos também a lugares que destacamos na nossa aldeia
+            como “pontos de interesse”, como eiras para apreciar a natureza ou uma tasca para um
+            cafezinho.
+          </Text>
+          <Button
+            href="https://www.google.com/maps/d/u/0/viewer?mid=12uQ-54JxIuYFFuS8r99oAsB_rAsRxPbt&ll=39.65727979629874%2C-8.761598399999992&z=15&fbclid=PAAaaVJbs99rNer1I8PKnqJ8QnBj9PV6_zutPiZ79WWmN-KPMBzQCqVdYrDDg"
+            newTab={true}
+          >
+            Ver no Google Maps
+          </Button>
+        </section>
+      </main>
+    </>
+  );
 }
